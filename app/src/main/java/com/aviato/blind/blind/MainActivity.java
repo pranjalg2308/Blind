d2ee71dfbc2c097f26a0b4c25c687c1bb19f2ef9
Pranjal <pranjalg2308@gmail.com>
2018-10-19 23:54:12 +0530

    Layout Creat

--- app/src/main/java/com/aviato/blind/blind/MainActivity.java
+++ app/src/main/java/com/aviato/blind/blind/MainActivity.java
@@ -1,13 +1,31 @@
 package com.aviato.blind.blind;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvIp;
+    private TextView tvPort;
+    private Button bnConnect;
+    private ProgressBar pbConnecting;
+    private ImageButton ibDisconnect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initVar();
+    }
+
+    private void initVar() {
+        tvIp = findViewById(R.id.tv_ip);
+        tvPort = findViewById(R.id.tv_port);
+        bnConnect = findViewById(R.id.bn_connect);
+        pbConnecting = findViewById(R.id.connection_load);
+        ibDisconnect = findViewById(R.id.ib_disconnect);
     }
 }
